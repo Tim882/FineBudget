82feb7be2ed5a0ad88e9f6ad05ae328de8059f48
DESKTOP-60C2PGK\1 <Sharafutdinov.Timur@tatar.ru>
2025-02-21 15:48:41 +0300

    Clear code

--- FineBudget/Program.cs
+++ FineBudget/Program.cs
@@ -101,26 +101,3 @@ static Task WriteResponse(HttpContext context, HealthReport healthReport)
     return context.Response.WriteAsync(
         Encoding.UTF8.GetString(memoryStream.ToArray()));
 }
-
-public class ExceptionHandler : IExceptionHandler
-{
-    private readonly ILogger<ExceptionHandler> _logger;
-
-    public ExceptionHandler(ILogger<ExceptionHandler> logger)
-    {
-        _logger = logger;
-    }
-
-    public ValueTask<bool> TryHandleAsync(
-        HttpContext httpContext,
-        Exception exception,
-        CancellationToken cancellationToken)
-    {
-        var exceptionMessage = exception.Message;
-        _logger.LogError(
-            "Error Message: {exceptionMessage}, Time of occurrence {time}",
-            exceptionMessage, DateTime.UtcNow);
-
-        return ValueTask.FromResult(false);
-    }
-}
